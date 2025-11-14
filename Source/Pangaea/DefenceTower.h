5918c701c4ec6056c38ebf12895cb5f0f2352da0
Shamsul Arifin <arifinbd6@gmail.com>
2025-11-14 19:17:11 +0600

    Add project files.

---
+++ Source/Pangaea/DefenceTower.h
@@ -0,0 +1,73 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "DefenceTower.generated.h"
+
+UCLASS()
+class PANGAEA_API ADefenceTower : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADefenceTower();
+
+	UPROPERTY(EditAnywhere, Category = "Tower Params")
+	int HealthPoints = 100;
+
+	UPROPERTY(EditAnywhere, Category = "Tower Params")
+	int ShellDefence = 2;
+	UPROPERTY(EditAnywhere, Category = "Tower Params")
+	float AttackRange = 15.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Tower Params")
+	float ReloadInterval = 1.0f;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	int _HealthPoints;		//The tower's current health points
+	float _ReloadCountingDown;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Defence Tower", meta = (AllowPrivateAccess = "True"))
+	UBoxComponent* _BoxComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Defence Tower", meta = (AllowPrivateAccess = "True"))
+	UStaticMeshComponent* _MeshComponent;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	FORCEINLINE UBoxComponent* GetBoxComponent() const
+	{
+		return _BoxComponent;
+	}
+
+	FORCEINLINE UStaticMeshComponent* GetMeshComponent() const
+	{
+		return _MeshComponent;
+	}
+
+	UFUNCTION(BlueprintPure, Category = "Pangaea|Defence Tower", meta = (DisplayName = "Get HP"))
+	int GetHealthPoints();
+
+	UFUNCTION(BlueprintPure, Category = "Pangaea|Defence Tower")
+	bool IsDestroyed();
+
+	UFUNCTION(BlueprintPure, Category = "Pangaea|Defence Tower")
+	bool CanFire();
+
+	void Fire();
+	void Hit(int Damge);
+
+protected:
+	void DestroyProcess();
+
+};
---
+++ Source/Pangaea/PlayerAvatar.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PlayerAvatar.generated.h"
+
+UCLASS()
+class PANGAEA_API APlayerAvatar : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlayerAvatar();
+	UPROPERTY(EditAnywhere, Category = "PlayerAvatar Params")
+	int HealthPoints = 500;
+
+	UPROPERTY(EditAnywhere, Category = "PlayerAvatar Params")
+	float Strength = 10;
+
+	UPROPERTY(EditAnywhere, Category = "PlayerAvatar Params")
+	float Armor = 3;
+
+	UPROPERTY(EditAnywhere, Category = "PlayerAvatar Params")
+	float AttackRange = 6.0f;
+
+	UPROPERTY(EditAnywhere, Category = "PlayerAvatar Params")
+	float AttackInterval = 1.2f;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	int _HealthPoints;
+	float _AttaclCountingDown;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Pangaea|PlayerCharacter", meta = (DisplayName = "Get HP"))
+	int GetHealthPoints();
+
+	UFUNCTION(BlueprintCallable, Category = "Pangaea|PlayerCharacter")
+	bool IsKilled();
+
+	UFUNCTION(BlueprintCallable, Category = "Pangaea|PlayerCharacter")
+	bool CanAttack();
+
+	void Attack();
+	void Hit(int Damge);
+
+protected:
+	void DieProcess();
+
+};
