52292417c9cf6ed98a991632919ee0e8977e5132
Shamsul Arifin <arifinbd6@gmail.com>
2025-11-23 01:48:13 +0600

    Changed default pawn

--- Source/Pangaea/PangaeaGameMode.cpp
+++ Source/Pangaea/PangaeaGameMode.cpp
@@ -1,8 +1,18 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "PangaeaGameMode.h"
+#include "PangaeaPlayerController.h"
+#include "PangaeaCharacter.h"
+#include "UObject/ConstructorHelpers.h"
 
 APangaeaGameMode::APangaeaGameMode()
 {
-	// stub
+	PlayerControllerClass = APangaeaPlayerController::StaticClass();
+
+	static ConstructorHelpers::FClassFinder<APangaeaCharacter> PlayerPawnBPClass(TEXT("/Game/TopDownCPP/Blueprints/BP_PlayerAvatar"));
+	if(PlayerPawnBPClass.Class != nullptr)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+
 }
